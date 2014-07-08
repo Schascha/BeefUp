21e7af9538a6bb06d1294f870d92757b0bccf918
Schascha <skuenstler@gmx.de>
2014-07-08 20:46:59 +0200

    [TASK] Add private method "click". Prepare hash and selfCLose properties

--- jquery.beefup.js
+++ jquery.beefup.js
@@ -14,14 +14,16 @@
         trigger			: '.beefup-head',       // String: Name of the trigger element
         content			: '.beefup-body',       // String: Name of the collapsible content
         openClass		: 'open',				// String: Name of the class which shows if a accordion is triggered or not
-        openSingle		: false,				// Boolean: Open just one accordion at once
         animation		: 'slide',				// String: Set animation type, "slide" or "fade"
-        showSpeed		: 200,					// Integer: Set the speed of the show animation
-        hideSpeed		: 200,					// Integer: Set the speed of the hide animation
+        openSpeed		: 200,					// Integer: Set the speed of the open animation
+        closeSpeed		: 200,					// Integer: Set the speed of the close animation
         scroll			: false,				// Boolean: Scroll to accordion
         scrollSpeed     : 400,					// Integer: Set the speed of the scroll feature
         scrollOffset	: 0,					// Integer: Additional offset to accordion position
-        onInit			: function(){},			// Callback: Fires after the accordion s initially setup
+        openSingle		: false,				// Boolean: Open just one accordion at once
+        selfClose       : false,                // Boolean: Close on click outside
+        hash            : true,                 // Boolean: Close on click outside
+        onInit			: function(){},			// Callback: Fires after the accordions initially setup
         onOpen			: function(){},			// Callback: Fires after accordion opens content
         onClose			: function(){}			// Callback: Fires after accordion close content
     };
@@ -32,34 +34,45 @@
             vars = $.extend({}, $.beefup.defaults, options);
 
 		// Public methods
-        this.open = function ($this) {
-			var $content = $this.find(vars.content + ':first'),
+        this.open = function ($el) {
+            var $this = ($el && $el.length) ? $el : $obj,
+                $content = $this.find(vars.content + ':first'),
                 open = function () {
                     $this.addClass(vars.openClass);
                     $(this).css('overflow', '');
                     if (vars.onOpen) vars.onOpen($this);
                 };
-			if (vars.animation === 'slide') $content.slideDown(vars.showSpeed, open);
-			else $content.fadeIn(vars.showSpeed, open);
+			if (vars.animation === 'slide') $content.slideDown(vars.openSpeed, open);
+			else $content.fadeIn(vars.openSpeed, open);
 			return $obj;
 		};
-		this.close = function ($this) {
-            var $content = $this.find(vars.content + ':first'),
+		this.close = function ($el) {
+            var $this  = ($el && $el.length) ? $el : $obj,
+                $content = $this.find(vars.content + ':first'),
                 close = function () {
                     $this.removeClass(vars.openClass);
                     $(this).css('overflow', '');
                     if (vars.onClose) vars.onClose($this);
                 };
-			if (vars.animation === 'slide') $content.slideUp(vars.hideSpeed, close);
-			else $content.fadeOut(vars.hideSpeed, close);
+			if (vars.animation === 'slide') $content.slideUp(vars.closeSpeed, close);
+			else $content.fadeOut(vars.closeSpeed, close);
 			return $obj;
 		};
-		this.scroll = function($this) {
+		this.scroll = function ($this) {
 			$('html, body').animate({scrollTop: $this.offset().top + vars.scrollOffset}, vars.scrollSpeed);
 			return $obj;
-		};
-
-		return this.each(function () {
+		},
+        this.click = function ($this) {
+            if (vars.openSingle) $obj.close($obj.not($this));
+            if (!$this.hasClass(vars.openClass)) {
+                $obj.open($this);
+                if (vars.scroll) $obj.scroll($this);
+            } else {
+                $obj.close($this);
+            }
+        };
+        
+		return this.each(function (index) {
 			var $this = $(this);
 
 			if ($this.data('beefup')) return;
@@ -68,17 +81,33 @@
 			$this.not('.' + vars.openClass).find(vars.content + ':first').hide();
             if (vars.onInit) vars.onInit($this);
 
+            // Click event
 			$this.on('click', vars.trigger + ':first', function(e) {
 				e.preventDefault();
-				if (vars.openSingle) $obj.close($obj.not($this));
-				if (!$this.hasClass(vars.openClass)) {
-					$obj.open($this);
-					if (vars.scroll) $obj.scroll($this);
-				} else {
-					$obj.close($this);
-				}
+                $obj.click($this);
 			});
-		});
 
+            // Hash
+            if (vars.hash && $this.attr('id')) {
+				if ($this.is(window.location.hash)) $obj.open($this);
+				$(window).bind('hashchange', function () {
+					if ($this.is(window.location.hash)) $obj.open($this);
+				});
+			}
+
+            // Self close
+            if (vars.selfClose) {
+                $(document).on('click', function (e) {
+                    if ($(e.target).closest($obj).length === 0) {
+                        $obj.close($this.filter(vars.openClass));
+                    }
+                });
+            }
+		});
     };
+    
+
+
+   
+
 })(jQuery);
