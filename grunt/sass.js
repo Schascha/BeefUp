0e3b64cab88e21edb3f158c96283ef117efcf8eb
Sascha Künstler <sascha.kuenstler@moccu.com>
2021-04-06 10:53:29 +0200

    Update grunt sass task

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,11 +1,14 @@
 // https://github.com/sindresorhus/grunt-sass
 
 module.exports = {
-	options: {
-		outputStyle: 'expanded',
-		sourceMap: false
-	},
-	build: {
-		files: '<%= files.sassBuild %>'
+	sass: {
+		options: {
+			implementation: 'sass',
+			outputStyle: 'expanded',
+			sourceMap: false
+		},
+		build: {
+			files: '<%= files.sassBuild %>'
+		}
 	}
 };
