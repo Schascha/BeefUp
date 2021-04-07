1ef4d89d0f38076aa3cb806eb29d3193c5339f2b
Sascha Künstler <sascha.kuenstler@moccu.com>
2021-04-07 09:51:21 +0200

    Fix grunt sass task

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,14 +1,14 @@
 // https://github.com/sindresorhus/grunt-sass
 
+var sass = require('node-sass');
+
 module.exports = {
-	sass: {
-		options: {
-			implementation: 'sass',
-			outputStyle: 'expanded',
-			sourceMap: false
-		},
-		build: {
-			files: '<%= files.sassBuild %>'
-		}
+	options: {
+		implementation: sass,
+		outputStyle: 'expanded',
+		sourceMap: false
+	},
+	build: {
+		files: '<%= files.sassBuild %>'
 	}
 };
